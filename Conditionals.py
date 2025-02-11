a15867aa7a8fe64acfc7a5db533c950e3e1b762c
bilal753632 <753632@pdsb.net>
2025-02-11 14:24:39 -0500

    Update Conditionals.py

--- Conditionals.py
+++ Conditionals.py
@@ -1,19 +1,18 @@
-import math
-
-x = int(input("Please input a whole number between 1 and 20: "))
+x = input("Please input a whole number: ")
+x = int(x)
+y = input("Please input another nonzero whole number")
+y = int(y)
 
 if 1 <= x <= 20:
-    y = int(input("Please input another whole number between 1 and 20: "))
-    
+
     if 1 <= y <= 20:
-        print("Now deciding if", y, "is a factor of", x, "...")
-        if y != 0:
-            rem = x % y
-            if rem == 0:
-                print("Yes!", y, "is a factor of", x)
+        print("Now deciding if", y, "is a factor of", x)
+
+        if x % y == 0:
+            print("Yes!", y, "is a factor of", x)
         else:
-            print("You cannot enter zero. A factor could not be determined.")
+            print("No!", y, "is not a factor of", x)
     else:
-        print("The second number must be between 1 and 20.")
+        print("Error: The second number must be between 1 and 20")
 else:
-    print("The first number must be between 1 and 20.")
+    print("Error: The first number must be between 1 and 20")
