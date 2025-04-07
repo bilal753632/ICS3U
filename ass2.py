cf347e6f56fd3132c39e34ada3cf9ab375f5353e
bilal753632 <753632@pdsb.net>
2025-04-07 13:27:20 -0400

    Update ass2.py

--- ass2.py
+++ ass2.py
@@ -1,18 +1,42 @@
 import random
 
-a = random.randrange(1,100) 
+# Generate a random number between 1 and 100 (inclusive)
+a = random.randrange(1, 101)
+
+# Introduction and instructions
 print("Hello! Welcome to the number guessing game!")
-print("I am thinking of a number between 1 and 100. It is your turn to guess what it is. You have a maximum of six (6) tries.")
-c = 1 
+print("I am thinking of a number between 1 and 100.")
+print("You have a maximum of six (6) tries.")
+
+# Initialize guess counter and user's guess variable
+c = 1
 x = 0
-while c < 7 and x != a:
-    x = int(input("Guess #" + str(c) + ": "))
+
+# Loop runs while the user still has guesses left and hasn't guessed the number
+while c <= 6 and x != a:
+    try:
+        # Prompt the user to make a guess
+        x = int(input(f"Guess #{c}: "))
+    except ValueError:
+        # Handle non-integer input gracefully
+        print("Please enter a valid integer.")
+        continue  # Skip the rest of the loop and prompt again
+
+    # Provide a hint if the guess is too low
     if x < a:
-       print("Higher")
-    if x > a:
-       print("Lower")
-    if x == a:
-       print("You Guessed right!")
+        print("Higher")
+    # Provide a hint if the guess is too high
+    elif x > a:
+        print("Lower")
+    # Congratulate the user if they guessed correctly
+    else:
+        print("You guessed right!")
+        break  # Exit the loop since the correct number was guessed
+
+    # Increment the guess counter
     c += 1
-if c >= 7 and x != a:
-    print("you are out of guesses! The answer was" , a , ". Better luck next time!")
+
+# If the loop ends without a correct guess, reveal the answer
+if x != a:
+    print(f"You are out of guesses! The answer was {a}. Better luck next time!")
+
